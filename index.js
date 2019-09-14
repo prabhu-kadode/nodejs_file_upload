e8bcacb857a6dd948eb92ee2cc339a50800e8a8f
Prabhu <prabhukadode1@gmail.com>
2019-09-14 22:11:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ console.log("simple, hi hello how are");
 console.log("simple, hi hello how are");
 console.log("simple, hi hello how are");
 console.log("simple, hi hello how are");
+console.log("simple, hi hello how are");
+alert("hishshs");
+alert("hishshs");
+alert("hishshs");
+alert("hishshs");
+alert("hishshs");
+console.log("simple, hi hello how are");
+
+console.log("simple, hi hello how are");
+
+console.log("simple, hi hello how are");
+console.log("simple, hi hello how are");
+console.log("simple, hi hello how are");
